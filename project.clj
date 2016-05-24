3f7a0f72412e5c396fd15df95035068231ae9717
Daniel Carleton <dacc@uw.edu>
2016-05-24 16:25:51 -0700

    deploy

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "https://github.com/pbostrom/lein-cljsasset"
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[s3-wagon-private "1.2.0"]]
+  :repositories {"snapshots" {:url        "s3p://management.maven-repo-bucket/snapshots"
+                              :username   :env/aws_access_key_id
+                              :passphrase :env/aws_secret_access_key}}
+  :deploy-repositories [["snapshots" :snapshots]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]])
 
